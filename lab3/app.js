77763c45208c8d3989260df430a5b50098e2e7c2
Nguyen Huy Hieu <hieunhgch18038@gmail.com>
2022-09-24 16:07:00 +0700

    upadte

--- lab3/app.js
+++ lab3/app.js
@@ -1,24 +1,34 @@
-//B1: khai bao http module (chay web)
 const http = require("http");
-//B2: khai bao port cua web server
-//Note: muốn deploy lên hosting (Heroku)
-//thì cần phải set port với process.env.PORT
-const port = process.env.PORT || 8000;
-//B3 tao server
-//request : client => server
-//response : server => client
-const app = http.createServer((req, res) => {
-    res.statusCode = 200; //HTTP_OK
+const fs = require("fs");
+http
+  .createServer((req, res) => {
+    res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
-    res.write("<h1>Hello everyone</h1>")
-    res.write("<h2>I am learning NodeJS</h2>");
-    res.write(
-      "<h3>My web page has been deployed to Heroku successfully !</h3>");
-    res.write("<h3>test nodemon</h3>")
-    res.end();
-})
-//B4 chay web
-//note: console.log ko bat buoc
-app.listen(port, () => {
-    console.log("Server is listening at http://localhost:" + port);
-});
+    if (req.url === "/") {
+      fs.readFile("./index.html", (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          res.write(data);
+          res.end;
+        }
+      });
+    } else if (req.url === "/greenwich") {
+      fs.readFile("./greenwich.html", (err, data) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.write(data);
+          res.end;
+        }
+      });
+    } else if (req.url === "/cloud") {
+      fs.readFile("./cloud.html", (err, data) => {
+        res.write(data);
+        res.end;
+      });
+    } else {
+      res.end("<h1>404 : Page not found</h1>");
+    }
+  })
+  .listen(process.env.PORT || 8000);
